c301a00e91b59cc89f0a3f6ec548505d2f8aa60f
tonismacbook <jdada@ggc.edu>
2025-07-01 18:55:52 -0400

    adding secret key

--- PyBlog/settings.py
+++ PyBlog/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-)@&62s4g*0859*^9f1k#)0i2wm@cj#u2muz@(^gf7sj#@e6ic7"
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
