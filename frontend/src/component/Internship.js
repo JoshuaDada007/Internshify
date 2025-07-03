364e947d00f6ea7c00bfba8c0c3b0eddd532b9bf
tonismacbook <jdada@ggc.edu>
2025-07-03 16:04:12 -0400

    fixed minor bug issues

--- frontend/src/component/Internship.js
+++ frontend/src/component/Internship.js
@@ -31,7 +31,7 @@ export function Internship() {
 
     async function getInternships() {
         try {
-            const response = await axios.get("https://internshify.onrender.com/all_internships_test");
+            const response = await axios.get("https://internshify.onrender.com/blogapp/all_internships_test");
             const data = response.data.reverse();
             setInternships(data);
             if (data[0]) {
